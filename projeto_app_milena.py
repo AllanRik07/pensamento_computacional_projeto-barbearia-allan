47f839ae76a0abfa3d8bdcf007e585a16b4114ea
AllanRik07 <allanrichard2911@gmail.com>
2026-03-23 17:06:39 -0300

    Refactor CRUD operations and menu structure
    correção

--- projeto_app_milena.py
+++ projeto_app_milena.py
@@ -8,30 +8,24 @@ Barbearia
  consulta, atualização e exclusão de dados.
 
 
- print("\n=== BARBEARIA ===")
- print("1. Cadastrar Cliente")
- print("2. Meu Perfil")
- print("3. Agendar Serviço")
- print("4. Meus Agendamentos")
- print("5. Serviços e Preços")
- print("6. Escolher Barbeiro")
- print("7. Histórico e Pagamentos")
- print("8. Avaliações")
- print("9. Notificações")
- print("0. Sair")
+ 
 
-while true:
 
-escolha = input("\nEscolha uma opção: ")
+while True:
 
-   if escolha == '1':
+escolha_menu = input("\nEscolha uma opção: ")
+
+   if escolha_menu == '1':
  
 print("Cadastrar Cliente...")
 
  
 '''
 
+# esse é o projeto correto
 
+
+# chama a mirla 
 print('Hello world!')
 
 # input('Precione o Enter para sair...')
@@ -47,4 +41,80 @@ nome_conta = input ('Digite sua conta:')
 print(f'{nome_conta} Correto agora entre com sua senha ')
 input  ('Digite sua senha:')
 
-acesso_menu = print ('Acesso Permitido')
+nome_cliente = print ('Acesso permitido')
+print ("escolha_menu")
+
+print("\n=== BARBEARIA ===")
+print("1. Cadastrar Cliente")
+print("2. Agendar Serviço")
+print("3. Meus Agendamentos")
+print("4. Serviços e Preços")
+print("5. Escolher Barbeiro")
+print("6. Histórico e Pagamentos")
+print("7. Avaliações")
+print("8. Notificações")
+print("0. Sair")
+
+
+
+while True:
+
+    escolha_menu = input("\nEscolha uma opção: ")
+    if escolha_menu =='1':
+
+        print("cadastrando cliente...")
+        nome_cliente = input("digite o nome do cliente: ")
+        telefone_cliente = input("digite o telefone do cliente:" )
+
+    
+    elif escolha_menu =='2':
+
+        print("perfil do cliente")  
+        nome_cliente = input("nome completo do cliente:")
+        nome_cliente = input("produtos que o cliente prefere:")
+
+    elif escolha_menu == '3':
+
+        print("Meus Agendamentos")
+        nome_cliente = input("digite o nome do cliente para exibir agenda:")
+        senha_cliente = input("digite a senha do cliente para confirmar o horário agendado:")
+
+    elif escolha_menu == '4':
+
+        print("Serviços e Preços")
+        print("1. Corte de Cabelo: 30.00")
+        print("2. Barba: 20.00")
+        print("3. Corte + Barba: 45.00")
+        print("4. Sobrancelha: 15.00")
+
+    elif escolha_menu == '5':
+
+        print("Escolher Barbeiro")
+        print("1. Barbeiro Allan")
+        print("2. Barbeiro Yuri")
+        print("3. Barbeira Milena")
+        
+    elif escolha_menu == '6':
+
+        print("Histórico e Pagamentos")
+        nome_cliente = input("digite o nome e sobrenome do cliente para exibir histórico de serviço e pagamentos:")
+        senha_cliente = input("digite a senha do cliente para confirmar o acesso ao histórico:")
+
+    elif escolha_menu == '7':
+
+        print("Avaliações")
+        nome_cliente = input("digite o nome do barbeiro (ex: Barbeiro Allan) para avaliar o serviço:")
+        avaliacao_cliente = input("digite sua avaliação (1 a 5 estrelas):")
+
+    elif escolha_menu == '8':
+        print("Notificações")
+        nome_cliente = input("digite o nome do cliente para exibir notificações:")
+
+
+    elif escolha_menu == '0':
+
+        print("Saindo do sistema. Até logo!")
+        break
+
+    else:
+        print("Opção inválida. Por favor, tente novamente.")
